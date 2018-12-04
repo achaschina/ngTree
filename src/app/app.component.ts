7d90ff9ae0472eb73a1c280f7f3cbd30187824f9
chashchina_av <chashchina_av@taximaxim.local>
2018-12-04 11:27:23 +0200

    angular tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {BehaviorSubject, Observable, of as observableOf} from 'rxjs';
+
+
+@Injectable()
+export class FileDatabase {
+  dataChange = new BehaviorSubject<FileNode[]>([]);
+
+  get data(): FileNode[] { return this.dataChange.value; }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    const dataObject = JSON.parse(TREE_DATA);
+    console.log(dataObject);
+
+    const data = this.buildFileTree(dataObject, 0);
+
+    this.dataChange.next(data);
+  }
+
+  buildFileTree(obj: {[key: string]: any}, level: number): FileNode[] {
+    return Object.keys(obj).reduce<FileNode[]>((accumulator, key) => {
+      const value = obj[key];
+      const node = new FileNode();
+      node.filename = key;
+
+      if (value != null) {
+        if (typeof value === 'object') {
+          node.children = this.buildFileTree(value, level + 1);
+        } else {
+          node.type = value;
+        }
+      }
+
+      return accumulator.concat(node);
+    }, []);
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FileDatabase]
 })
 export class AppComponent {
-  title = 'table-tree';
+  treeFlattener: MatTreeFlattener<FileNode, FileFlatNode>;
+  treeControl: FlatTreeControl<FileFlatNode>;
+  dataSource: MatTreeFlatDataSource<FileNode, FileFlatNode>;
+
+  constructor(database: FileDatabase) {
+    this.treeFlattener = new MatTreeFlattener(this.transformer, this._getLevel,
+      this._isExpandable, this._getChildren);
+    this.treeControl = new FlatTreeControl<FileFlatNode>(this._getLevel, this._isExpandable);
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+    console.log(this.treeControl, 555)
+
+    database.dataChange.subscribe(data => {
+      this.dataSource.data = data;
+    });
+
+  }
+
+  transformer = (node: FileNode, level: number) => {
+    return new FileFlatNode(!!node.children, node.filename, level, node.type, 55, '94949494');
+  }
+
+  private _getLevel = (node: FileFlatNode) => node.level;
+
+  private _isExpandable = (node: FileFlatNode) => node.expandable;
+
+  private _getChildren = (node: FileNode): Observable<FileNode[]> => observableOf(node.children);
+
+  hasChild = (_: number, _nodeData: FileFlatNode) => _nodeData.expandable;
 }
+
+export class FileFlatNode {
+  constructor(
+    public expandable: boolean,
+    public filename: string,
+    public level: number,
+    public type: any,
+    public rowCounter: number,
+    public version: string) {}
+}
+
+export class FileNode {
+  children: FileNode[];
+  filename: string;
+  type: any;
+}
+
+const TREE_DATA = JSON.stringify({
+  Applications: {
+    Calendar: 'app',
+    CalendarT: {
+      type: 'app',
+      rowCounter: 125
+    },
+    Chrome: 'app',
+    Webstorm: 'app'
+  },
+  Documents: {
+    angular: {
+      src: {
+        compiler: 'ts',
+        core: 'ts'
+      }
+    },
+    material2: {
+      src: {
+        button: 'ts',
+        checkbox: 'ts',
+        input: 'ts'
+      }
+    }
+  },
+  Downloads: {
+    October: 'pdf',
+    November: 'pdf',
+    Tutorial: 'html'
+  },
+  Pictures: {
+    'Photo Booth Library': {
+      Contents: 'dir',
+      Pictures: 'dir'
+    },
+    Sun: 'png',
+    Woods: 'jpg'
+  }
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {CdkTableModule} from '@angular/cdk/table';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule, MatButtonToggleModule } from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CdkTreeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    CdkTableModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
